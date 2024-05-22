c02d867e3e205bcc20f3c6a1839dc814b484d116
Yhordi Choque <yordychoque124@gmail.com>
2024-05-21 21:13:45 -0400

    creamos nuevas facturas

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({ // valida que los datos ingresados sean correctos
+    id: z.string(),
+    customerId: z.string(),
+    amount:z.coerce.number(),
+    status: z.enum(['pending','paid']),
+    date:z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({id:true, date:true});
+
+export async function createInvoice(formData: FormData){
+    const {customerId, amount, status} =  CreateInvoice.parse ( {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status:formData.get('status'),
+
+    })
+
+    // Test it out
+const amountInCents = amount * 100;
+const date = new Date().toISOString().split('T')[0];
+ 
+await sql`
+   INSERT INTO invoices (customer_id, amount,status, date)
+   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+ `;
+
+ revalidatePath('/page/dashboard/invoices');
+redirect('/page/dashboard/invoices')
+}
